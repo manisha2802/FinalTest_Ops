c144c12256e2966ca65accbcfca8155499b2e088
manisha.kamble <manisha.kamble@INBL01LAP30379.brd.g-c-i.com>
2023-06-15 17:01:45 +0530

    commit1

---
+++ Test Suites/Test Suite1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>22db55a0-57f9-4c4e-ba8e-fca299e27919</testSuiteGuid>
+   <testCaseLink>
+      <guid>4fb8675e-e93c-43fa-aaeb-ba3e9e1e1145</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
